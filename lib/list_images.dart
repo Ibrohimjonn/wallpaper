01556e38f4222bdd6c986499337d9dd7d2ee12ad
Ibrohimjonn <gurilinfraligov@gmail.com>
2021-05-29 21:43:03 +0500

    rasmlar o'zgardi

--- lib/list_images.dart
+++ lib/list_images.dart
@@ -1,4 +1,14 @@
 List<String> data = [
-  'https://avatars.mds.yandex.net/get-zen_doc/1532998/pub_5e67422ebf998668491f86a0_5e67bb2d15e73325d7634bc0/scale_1200',
+  'https://i.pinimg.com/originals/86/af/6c/86af6caf226959ecfe1194991c1379ce.png',
   'https://data.whicdn.com/images/330118478/original.jpg',
+  'https://avatars.mds.yandex.net/get-zen_doc/1532998/pub_5e67422ebf998668491f86a0_5e67bb2d15e73325d7634bc0/scale_1200',
+  'https://i.pinimg.com/originals/8f/51/60/8f51608b351c7ac7046d93d7995b1e61.jpg',
+  'https://i.pinimg.com/originals/a1/52/af/a152af13851decccd81684f482a9c6ec.jpg',
+  'https://pm1.narvii.com/7191/3e520ce072bc407e3d5b04b1c32e8d61174aea30r1-1080-1351v2_hq.jpg',
+  'https://i.pinimg.com/originals/5b/36/75/5b36759317b0aa0b9841b48b366a5197.jpg',
+  'https://pbs.twimg.com/media/ENgohqXU4AAQ3Xs.jpg:large',
+  'https://static.asiachan.com/Jennie.Kim.full.130818.jpg',
+  'https://i.pinimg.com/736x/36/36/a7/3636a70706afb87a03a7640fb9ebcd76.jpg',
+  'https://i.pinimg.com/originals/b6/85/8c/b6858cd5e10df087c612f0d848aba188.jpg',
+  'https://pbs.twimg.com/media/D05kMqvUYAEYs4q.jpg',
 ];
